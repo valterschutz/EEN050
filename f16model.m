7e6dcc7cdeea65f417f30c256677f74671497eaa
eliasstenhede <72222347+eliasstenhede@users.noreply.github.com>
2023-09-14 17:13:00 +0200

    zoom session

--- f16model.m
+++ f16model.m
@@ -59,52 +59,56 @@ Gc = (A-B*K(:,1:5));
 %% A2
 s = tf("s");
 %% Wr
-Wr = [6.25^2/(s^2+2*6.25*s+6.25^2); 0; 0]
+Wr = [6.25^2/(s^2+2*6.25*s+6.25^2); 0; 0];
 %% Wd
-Wd = [0; (0.9751*s+0.2491)/(s^2+0.885*s+0.1958)]
+Wd = [0; (0.9751*s+0.2491)/(s^2+0.885*s+0.1958)];
 %% We
 gdc = 400;
 wc = 4.3;
 ghf = 0.4;
 [k,z,p] = getparams(gdc,ghf,wc);
-We = [k * (s + z)/(s + p) 0 0]
+We = [k * (s + z)/(s + p) 0 0];
 %% Wpalpha
 gdc = 2.5;
 wc = 0.45;
 ghf = 0.015;
 [k,z,p] = getparams(gdc,ghf,wc);
-Wpalpha = k * (s + z)/(s + p)
-%% Wpan
+Wpalpha = k * (s + z)/(s + p);
+evalfr(Wpalpha, 0.0)/evalfr(Wpalpha, 0.45)
+%% Wp
 gdc = 2.5;
 wc = 0.7;
 ghf = 0.0063;
 [k,z,p] = getparams(gdc,ghf,wc);
-Wpan = k * (s + z)/(s + p)
+Wpan = k * (s + z)/(s + p);
 
 Wp = [Wpalpha, 0, Wpan];
-%% Wm1
+evalfr(Wpan, 0.0)/evalfr(Wpan, 0.7)
+%% Wm
 gdc = 0.2;
 wc = 26;
 ghf = 3;
 [k,z,p] = getparams(gdc,ghf,wc);
-Wm1 = k * (s + z)/(s + p)
-%% Wm2
+% top diag
+Wm1 = k * (s + z)/(s + p);
 gdc = 0.16;
 wc = 42;
 ghf = 2;
 [k,z,p] = getparams(gdc,ghf,wc);
-Wm2 = k * (s + z)/(s + p)
+% bottom diag
+Wm2 = k * (s + z)/(s + p);
+% full matrix
+Wm = blkdiag(Wm1, Wm2);
 %% Wu
-Wu = [0 1/(35*pi/180)]
+Wu = [0 1/(35*pi/180)];
 %% Wn
-Wn = [0.001*180/pi; 0.001; 0.001*3.28084]
+Wn = [0.001*180/pi; 0.001; 0.001*3.28084];
 %% A2 E2
-Wm = blkdiag(Wm1, Wm2)
 
 
 P_11 = [0 0; 0 0];
 P_12 = [0 0 0; 0 0 0];
-P_13 = [Ga_tf];
+P_13 = Ga_tf;
 P_21 = [-We*Gn_tf*Wm           ; Wp*Gn_tf*Wm       ; Wu*Wm];
 P_22 = [We*Wr -We*Gn_tf*Wd 0   ; 0 Wp*Gn_tf*Wd 0   ; 0 Wu*Wd 0];
 P_23 = [-We*Gn_tf*Ga_tf        ; Wp*Gn_tf*Ga_tf    ; Wu*Ga_tf    ];
@@ -113,9 +117,11 @@ P_32 = [1 0 0                  ; [0; 0; 0] Gn_tf*Wd  Wn];
 P_33 = [0 0                    ; Gn_tf*Ga_tf      ];
 
 P = [P_11, P_12, P_13;P_21 P_22 P_23;P_31 P_32 P_33];
-nmeas=3;
+nmeas=4;
 ncont=2;
-%[K,CL,gamma] = hinfsyn(P, nmeas, ncont)
-[K,CL,gamma] = h2syn(P, nmeas, ncont)
+
+[K,CL,gamma,info] = hinfsyn(P, nmeas, ncont);
 K
 %% A2 E3
+[K,CL,gamma,info] = hinfsyn(P, nmeas, ncont);
+K
--- getparams.m
+++ getparams.m
@@ -1,5 +1,7 @@
 function [k,z,p] = getparams(gdc,ghf,wc)
-p = sqrt(wc^2/(2*gdc^2/ghf - 1));
+%p = sqrt(wc^2/(2*gdc^2/ghf^2 - 1));
+%
+p = 2.4*wc * sqrt(1 - 2*ghf^2/gdc^2);
 k = ghf;
 z = p*gdc/ghf;
 end
