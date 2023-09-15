c0fb842aaa7ba7b537b49c4866ff0e09d90ae8ed
eliasstenhede <72222347+eliasstenhede@users.noreply.github.com>
2023-09-15 11:00:10 +0200

    kul grejer

--- f16model.m
+++ f16model.m
@@ -129,3 +129,14 @@ gamma
 %% A2 E3
 %[K,CL,gamma,info] = hinfsyn(P, nmeas, ncont);
 %K
+
+function [k,z,p] = getparams(gdc,ghf,wc)
+    if (gdc > ghf)
+        p = wc * sqrt(1 - 2*ghf^2/gdc^2);
+    else
+        p = wc * sqrt(1 - 2*gdc^2/ghf^2);
+        %p = wc * sqrt((ghf^2-2)/(2*gdc^2/ghf^2-ghf^2));
+    end
+    k = ghf;
+    z = p*gdc/ghf;
+end
